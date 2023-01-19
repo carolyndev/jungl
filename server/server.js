89b1a57811984f745009dec60fb9ac80d8254716
Carolyn Lau <thecarolynlau@gmail.com>
2023-01-19 15:41:13 -0800

    Added post route

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-const express = require('express')
-// const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+// const db = require('./db');
 
-const db = require('./db')
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const app = express()
-const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-// app.use(bodyParser.json())
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/api', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED' });
+  res.status(200).send({express: 'YOUR EXPRESS BACKEND IS CONNECTED'});
+});
+
+app.post('/api/:id', (req, res) => {
+  const {id} = req.params;
+  const {text} = req.body;
+
+  if (!text) return res.status(418).send({message: 'You need a body.text'});
+  res.send({item: `The item id is ${id} with a text of: ${text}.`});
 });
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
