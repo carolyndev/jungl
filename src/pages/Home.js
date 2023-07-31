cd7c430c8d2a5a49823024ebc37fa66078f417de
Carolyn Lau <thecarolynlau@gmail.com>
2023-07-30 22:52:20 -0700

    Update home page to use plant service

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,28 @@
-import axios from 'axios';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Hero from '../components/Hero';
 import Info from '../components/Info';
 import Collection from '../components/Collection';
-import {collections, guides} from '../helpers/data';
+import {guides} from '../helpers/data';
+import * as plantService from '../services/plant.service';
 
 const Home = (props) => {
 	const {selected, setSelected, itemToAdd, setItemToAdd} = props;
+	const [collections, setCollections] = useState([]);
 
-	useEffect(() => {
-		axios.get('http://localhost:8888/.netlify/functions/api')
-			.then(function (response) {
-				console.log(response);
-			})
-			.catch(function (error) {
-				console.log(error);
+	useEffect(async () => {
+		await plantService.getPlants()
+			.then(response => {
+				if (response.status !== 200) return
+				setCollections(
+					[{
+						title: 'test title',
+						bannerImg:
+							'https://images.unsplash.com/photo-1605449669747-35d71b9436f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
+						items: response.data.plants,
+						action: 'Shop Now ⟶',
+						url: 'tools',
+					}]
+				);
 			});
 	}, []);
 
